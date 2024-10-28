16b91bc3adbeec08ff6a72200473ce1ce80d6b28
haseeb-ship <qx.campp@gmail.com>
2024-10-28 19:22:41 +0500

    Parts of Speech Tagging.py

--- Parts of Speech Tagging.py
+++ Parts of Speech Tagging.py
@@ -2,7 +2,9 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk import pos_tag
 
-
+# Download necessary NLTK resources
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 # Step 1: Generate a large text corpus (simulated)
 sample_sentences = [
